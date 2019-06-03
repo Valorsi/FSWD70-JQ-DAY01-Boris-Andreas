573188f5a2fa5c826991efbc0b80c12b10d61183
Boris Jankovic <Borisjanko@outlook.com>
2019-06-03 11:57:02 +0200

    Add Exercise

--- Ex2B/myscript.js
+++ Ex2B/myscript.js
@@ -1,8 +1,27 @@
 $("form").submit(function(e) {
 
 	e.preventDefault();
-	
-	
-	
 
 });
+	
+	// Disappear form
+// 	$(document).ready(function(){
+// 	$("form").remove();
+// });
+
+// add 5 Divs and classes
+// $(document).ready(function(){
+// 	var newDiv= $('<div></div><div></div><div></div><div></div><div></div>')
+// 	$("article").append(newDiv);
+// 	$("article div").toggleClass("box");
+// });
+
+//change value
+// $(document).ready(function(){
+// 	$("#msg").val("Search for....")
+// });
+
+//change link
+// $(document).ready(function(){
+// 	$("a").attr('href', 'http://www.codefactory.wien' );
+// });
--- FSWD70-JQ-DAY01-Boris-Andreas/Ex2B/myscript.js
+++ FSWD70-JQ-DAY01-Boris-Andreas/Ex2B/myscript.js
@@ -6,3 +6,25 @@ $("form").submit(function(e) {
 	
 
 });
+
+	// Disappear form
+// 	$(document).ready(function(){
+// 	$("form").remove();
+// });
+
+// add 5 Divs and classes
+// $(document).ready(function(){
+// 	var newDiv= $('<div></div><div></div><div></div><div></div><div></div>')
+// 	$("article").append(newDiv);
+// 	$("article div").toggleClass("box");
+// });
+
+//change value
+// $(document).ready(function(){
+// 	$("#msg").val("Search for....")
+// });
+
+//change link
+// $(document).ready(function(){
+// 	$("a").attr('href', 'http://www.codefactory.wien' );
+// });
